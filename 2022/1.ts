ee9cb280bc0424635854b14db423c3c57040c637
kiko-g <kikojpgoncalves@gmail.com>
2022-12-01 15:12:17 +0200

    add: 2022, day 1, part 2

--- 2022/1.ts
+++ 2022/1.ts
@@ -1,13 +1,19 @@
 const input = await Deno.readTextFile("./input/1.txt")
 
-let max = 0
+const elfCaloriesTotals = []
 const elvesCaloriesRaw = input.split("\n\n")
 
 for (let i = 0; i < elvesCaloriesRaw.length; i++) {
   const elfCalories = elvesCaloriesRaw[i].split("\n")
-  const elfCaloriesTotals = elfCalories.map((item) => parseInt(item))
-  const sum = elfCaloriesTotals.reduce((acc, value) => acc + value)
-  if (sum > max) max = sum
+  const elfCaloriesInt = elfCalories.map((item) => (item === "" ? 0 : parseInt(item)))
+  const elfCaloriesTotal = elfCaloriesInt.reduce((acc, value) => acc + value)
+  elfCaloriesTotals.push(elfCaloriesTotal)
 }
 
-console.log(max)
+const elfCaloriesTotalsSortedDesc = elfCaloriesTotals.sort().reverse()
+
+const max = elfCaloriesTotalsSortedDesc[0]
+const top3Total = elfCaloriesTotalsSortedDesc.splice(0, 3).reduce((acc, value) => acc + value)
+
+console.log("Max:", max)
+console.log("Top 3 Sum:", top3Total)
