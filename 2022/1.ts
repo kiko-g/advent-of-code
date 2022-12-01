70038f4b886d225ce3f88adbf8bb2d8aa710ad49
kiko-g <kikojpgoncalves@gmail.com>
2022-12-01 15:16:28 +0200

    refactor: rename variables

--- 2022/1.ts
+++ 2022/1.ts
@@ -1,19 +1,19 @@
 const input = await Deno.readTextFile("./input/1.txt")
 
-const elfCaloriesTotals = []
+const elvesTotals = []
 const elvesCaloriesRaw = input.split("\n\n")
 
 for (let i = 0; i < elvesCaloriesRaw.length; i++) {
-  const elfCalories = elvesCaloriesRaw[i].split("\n")
-  const elfCaloriesInt = elfCalories.map((item) => (item === "" ? 0 : parseInt(item)))
-  const elfCaloriesTotal = elfCaloriesInt.reduce((acc, value) => acc + value)
-  elfCaloriesTotals.push(elfCaloriesTotal)
+  const elf = elvesCaloriesRaw[i].split("\n")
+  const elfInt = elf.map((item) => (item === "" ? 0 : parseInt(item)))
+  const elfTotal = elfInt.reduce((acc, value) => acc + value)
+  elvesTotals.push(elfTotal)
 }
 
-const elfCaloriesTotalsSortedDesc = elfCaloriesTotals.sort().reverse()
+const totalsDesc = elvesTotals.sort().reverse()
 
-const max = elfCaloriesTotalsSortedDesc[0]
-const top3Total = elfCaloriesTotalsSortedDesc.splice(0, 3).reduce((acc, value) => acc + value)
+const max = totalsDesc[0]
+const totalTop3 = totalsDesc.splice(0, 3).reduce((acc, value) => acc + value)
 
 console.log("Max:", max)
-console.log("Top 3 Sum:", top3Total)
+console.log("Top 3 Sum:", totalTop3)
